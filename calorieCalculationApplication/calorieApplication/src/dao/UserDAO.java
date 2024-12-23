2ea40f5a94ccb3bbe9894c38ae83518758f50f91
unknown <aminasokoli0602@outlook.com>
2024-12-23 21:54:06 +0100

    permiresim metodash qe lidhen me veprimet me te dhenat e userit

--- calorieCalculationApplication/calorieApplication/src/dao/UserDAO.java
+++ calorieCalculationApplication/calorieApplication/src/dao/UserDAO.java
@@ -4,6 +4,8 @@ import entity.*;
 
 public interface UserDAO {
     public User getUserById(int id);
-    public User getUserByEmailAndPassword(String email, String password);
-
+    public User getUserByEmail(String email);
+    public boolean addUser(User user); //kthejme boolean per efekt abstraksioni
+    public boolean updateUser(User user);
+    public boolean deleteUser(int id);
 }
