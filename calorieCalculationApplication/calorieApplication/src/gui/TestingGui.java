4de3c06fc00d18d3d82d672ce2c640119b314e2b
unknown <aminasokoli0602@outlook.com>
2024-12-24 19:43:30 +0100

    krijimi i file-ave fxml dhe klasave perkatese per krijimin e registerFormit gui

---
+++ calorieCalculationApplication/calorieApplication/src/gui/TestingGui.java
@@ -0,0 +1,20 @@
+package gui;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
+public class TestingGui extends Application {
+
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Register");
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("RegisterView.fxml"))));
+        primaryStage.show();
+    }
+    public static void main(String[] args) {
+        launch();
+    }
+}
