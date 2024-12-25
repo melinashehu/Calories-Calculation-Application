8704585042250ff50243492e9e628fb75eaf3cf1
unknown <aminasokoli0602@outlook.com>
2024-12-25 17:06:11 +0100

    adding a to-do comment

--- calorieCalculationApplication/calorieApplication/src/entity/StatisticalReport.java
+++ calorieCalculationApplication/calorieApplication/src/entity/StatisticalReport.java
@@ -5,7 +5,7 @@ public class StatisticalReport {
     private double totalCaloriesConsumed;
     private double totalSpendingMoney;
     private int daysAboveCalorieThreshold;
-
+    //duhet shtuar atribut per muajt kur eshte kaluar 1000euro
     public StatisticalReport() {}
 
     public StatisticalReport(int userId, double totalCaloriesConsumed, int daysAboveCalorieThreshold) {
