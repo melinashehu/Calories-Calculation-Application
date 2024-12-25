6f306826729f5b6c4f463bcb9267138d5fb0b6cf
unknown <aminasokoli0602@outlook.com>
2024-12-25 14:15:53 +0100

    permiresim ne validimin e inputeve dhe rasteve specifike

--- calorieCalculationApplication/calorieApplication/src/validation/InputValidator.java
+++ calorieCalculationApplication/calorieApplication/src/validation/InputValidator.java
@@ -18,4 +18,24 @@ public class InputValidator {
     public static boolean isValidPassword(String password){
         return !isEmpty(password) && password.length()>=6;
     }
+
+    public static String isValidInput(String inputName, String inputEmail, String inputPassword) {
+        if(isEmpty(inputName) || isEmpty(inputEmail) || isEmpty(inputPassword)){
+            return "All fields must be filled.";
+        }
+
+        if(!isValidUserName(inputName))
+            return "Name must be at least 3 characters long.";
+
+
+        if(!isValidEmail(inputEmail))
+            return "Email must be a valid email address. \n example@domain.com";
+
+
+        if(!isValidPassword(inputPassword))
+            return "Password must be at least 6 characters long.";
+
+        return null;
+    }
+
 }
