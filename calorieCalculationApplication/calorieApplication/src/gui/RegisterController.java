25f87f0687c10c410dbf5dc9039d57d1d40e1638
unknown <aminasokoli0602@outlook.com>
2024-12-25 14:15:19 +0100

    design dhe registerForm perfundimtare

--- calorieCalculationApplication/calorieApplication/src/gui/RegisterController.java
+++ calorieCalculationApplication/calorieApplication/src/gui/RegisterController.java
@@ -16,28 +16,35 @@ public class RegisterController {
     @FXML
     private Button registerButton;
     @FXML
-    private Button cancelButton;
+    private Button closeButton;
 
     private void showError(String message){
         Alert alert = new Alert(Alert.AlertType.ERROR, message);
         alert.showAndWait();
     }
 
+    public void handleRegisterButtonMouseEntered(){
+        registerButton.setStyle("-fx-background-color: #6f9973; -fx-text-fill: white; -fx-background-radius:10;");
+    }
+    public void handleRegisterButtonMouseExited(){
+        registerButton.setStyle("-fx-background-color: #85b48a; -fx-text-fill:white; -fx-background-radius:10;");
+    }
+
+    public void handleCloseButtonMouseEntered(){
+        closeButton.setStyle("-fx-background-color: #e6e6e6; -fx-text-fill: #666666; -fx-background-radius: 10;");
+    }
+    public void handleCloseButtonMouseExited(){
+        closeButton.setStyle("-fx-background-color: #f2f2f2; -fx-text-fill: #999999; -fx-background-radius: 10;");
+    }
+
     public void handleRegister(){
         String name = nameField.getText();
         String email = emailField.getText();
         String password = passwordField.getText();
+        String validationMessage = InputValidator.isValidInput(name, email, password);
 
-        if(!InputValidator.isValidUserName(name)){
-            showError("Name must be at least 3 characters long.");
-            return;
-        }
-        if(!InputValidator.isValidEmail(email)){
-            showError("Email must be a valid email format. \n example@domain.com");
-            return;
-        }
-        if(!InputValidator.isValidPassword(password)){
-            showError("Password must be at least 6 characters long.");
+        if(validationMessage!= null){
+            showError(validationMessage);
             return;
         }
 
@@ -48,7 +55,7 @@ public class RegisterController {
             Alert alert = new Alert(Alert.AlertType.INFORMATION, "Registration successful!");
             alert.showAndWait();
         }else{
-            showError("Registration faled. Check you credentials.");
+            showError("Registration failed. Check you credentials.");
         }
     }
 
