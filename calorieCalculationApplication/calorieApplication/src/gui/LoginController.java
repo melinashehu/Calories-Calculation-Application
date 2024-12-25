f40aedb2c38bc6dec15b4ce9603b543ceb3a4d24
unknown <aminasokoli0602@outlook.com>
2024-12-25 14:36:44 +0100

    loginForm i perditesuar

--- calorieCalculationApplication/calorieApplication/src/gui/LoginController.java
+++ calorieCalculationApplication/calorieApplication/src/gui/LoginController.java
@@ -1,4 +1,48 @@
 package gui;
 
+import javafx.fxml.FXML;
+import dao.*;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import validation.*;
+import entity.*;
 public class LoginController {
+    @FXML
+    private TextField emailField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Button closeButton;
+    @FXML
+    private Label messageLabel;
+    private void showError(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR, message);
+        alert.showAndWait();
+    }
+    public void handleLogin(){
+        String email = emailField.getText();
+        String password = passwordField.getText();
+        if(!InputValidator.isValidEmail(email)){
+            showError("Email is invalid.");
+            return;
+        }
+        if(InputValidator.isEmpty(password)){
+            showError("Password is invalid.");
+            return;
+        }
+
+
+        UserDAOImplementation userDao = new UserDAOImplementation();
+        User user = userDao.getUserByEmailAndPassword(email, password);
+        if(user != null){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION, "Log-in successful! Welcome, "+user.getUserName());
+            alert.showAndWait();
+        }else{
+            showError("Invalid email or password.");
+        }
+    }
+
+
 }
