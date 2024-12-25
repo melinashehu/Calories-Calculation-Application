3ad23ccfa79b18f6868842018570306964823559
unknown <aminasokoli0602@outlook.com>
2024-12-25 16:24:47 +0100

    shtimi i ridrejtimit per ne registerForm kur klikohet hyperlink register

--- calorieCalculationApplication/calorieApplication/src/gui/LoginController.java
+++ calorieCalculationApplication/calorieApplication/src/gui/LoginController.java
@@ -2,10 +2,15 @@ package gui;
 
 import javafx.fxml.FXML;
 import dao.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
 import validation.*;
 import entity.*;
+
+import java.io.IOException;
+
 public class LoginController {
     @FXML
     private TextField emailField;
@@ -15,24 +20,22 @@ public class LoginController {
     private Button loginButton;
     @FXML
     private Button closeButton;
-    @FXML
-    private Label messageLabel;
+
+
     private void showError(String message) {
         Alert alert = new Alert(Alert.AlertType.ERROR, message);
         alert.showAndWait();
     }
+
     public void handleLogin(){
         String email = emailField.getText();
         String password = passwordField.getText();
-        if(!InputValidator.isValidEmail(email)){
-            showError("Email is invalid.");
-            return;
-        }
-        if(InputValidator.isEmpty(password)){
-            showError("Password is invalid.");
-            return;
-        }
+        String validationMessage = InputValidator.isValidInput(email, password);
 
+        if(validationMessage!= null){
+            showError(validationMessage);
+            return;
+        }
 
         UserDAOImplementation userDao = new UserDAOImplementation();
         User user = userDao.getUserByEmailAndPassword(email, password);
@@ -44,5 +47,29 @@ public class LoginController {
         }
     }
 
+    public void redirectToRegisterForm() throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("RegisterView.fxml"));
+        Scene registerScene = new Scene(loader.load());
+        Stage stage = (Stage)loginButton.getScene().getWindow();
+        stage.setScene(registerScene);
+        stage.show();
+    }
 
+    public void handleLoginButtonMouseEntered(){
+        loginButton.setStyle("-fx-background-color: #6f9973; -fx-text-fill: white; -fx-background-radius:10;");
+    }
+    public void handleLoginButtonMouseExited(){
+        loginButton.setStyle("-fx-background-color: #85b48a; -fx-text-fill:white; -fx-background-radius:10;");
+    }
+
+    public void handleLoginCloseButtonMouseEntered(){
+        closeButton.setStyle("-fx-background-color: #e6e6e6; -fx-text-fill: #666666; -fx-background-radius: 10;");
+    }
+    public void handleLoginCloseButtonMouseExited(){
+        closeButton.setStyle("-fx-background-color: #f2f2f2; -fx-text-fill: #999999; -fx-background-radius: 10;");
+    }
+
+    public void closeLoginApp(){
+        System.exit(0);
+    }
 }
