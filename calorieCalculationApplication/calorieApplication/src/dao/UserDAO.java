b90261e8f2935bfd281da252a502e7b9aa5cf870
unknown <aminasokoli0602@outlook.com>
2024-12-25 14:12:52 +0100

    ndryshimi i metodes getUserByEmail ne getUserByEmailAndPassword per saktesi gjate logimit

--- calorieCalculationApplication/calorieApplication/src/dao/UserDAO.java
+++ calorieCalculationApplication/calorieApplication/src/dao/UserDAO.java
@@ -4,7 +4,7 @@ import entity.*;
 
 public interface UserDAO {
     public User getUserById(int id);
-    public User getUserByEmail(String email);
+    public User getUserByEmailAndPassword(String email, String password);
     public boolean addUser(User user); //kthejme boolean per efekt abstraksioni
     public boolean updateUser(User user);
     public boolean deleteUser(int id);
