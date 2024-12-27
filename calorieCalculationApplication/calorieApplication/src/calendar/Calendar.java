1d02879dd73a057366bfd627578f2d60202440b0
Melina <melinashehu1234@gmail.com>
2024-12-27 16:17:49 +0100

    Shtimi i metodes per llogaritjen e dates aktuale

--- calorieCalculationApplication/calorieApplication/src/calendar/Calendar.java
+++ calorieCalculationApplication/calorieApplication/src/calendar/Calendar.java
@@ -6,10 +6,17 @@ import java.time.LocalDate;
 import java.time.ZoneId;
 
 public class Calendar {
+
     public static java.sql.Date calculateEndWeekDate(Date startingDate){
         LocalDate localStartDate = startingDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
         LocalDate localEndDate = localStartDate.plusDays(7);
         return (Date) Date.from(localEndDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
 
+    }
+
+    // Kjo metodë merr datën aktuale dhe e kthen si java.sql.Date
+    public static java.sql.Date getCurrentDate(){
+        LocalDate currentDate = LocalDate.now();  // Merr datën aktuale
+        return Date.valueOf(currentDate);  // Kthen datën e sotme si java.sql.Date
     }
 }
