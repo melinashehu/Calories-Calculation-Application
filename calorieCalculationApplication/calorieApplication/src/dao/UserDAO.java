4d11dbbf785dfdbf854228bb297b04d24b1cb3dc
Edna Brahimaj <Edna.Brahimaj@fti.edu.al>
2024-12-29 19:09:57 +0100

    Shtimi i metodes qe llogarit te gjithe ID-te e user-ave

--- calorieCalculationApplication/calorieApplication/src/dao/UserDAO.java
+++ calorieCalculationApplication/calorieApplication/src/dao/UserDAO.java
@@ -9,4 +9,5 @@ public interface UserDAO {
     public boolean addUser(User user);
     public boolean updateUser(User user);
     public boolean deleteUser(int id);
+    public List<Integer> getAllUsersIds();
 }
