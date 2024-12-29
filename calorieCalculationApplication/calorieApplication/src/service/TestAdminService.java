f72512a8a0af44d1d1acf4222a4e2c05ea94a925
Edna Brahimaj <Edna.Brahimaj@fti.edu.al>
2024-12-29 19:14:44 +0100

    Testimi i klases AdminService per te kontrolluar funksionimin e metodave

---
+++ calorieCalculationApplication/calorieApplication/src/service/TestAdminService.java
@@ -0,0 +1,75 @@
+package service;
+
+import Report.UserReport;
+import calendar.Calendar;
+import dao.UserDAOImplementation;
+import entity.User;
+import login.UserSession;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Scanner;
+
+public class TestAdminService {
+
+    public static void main(String[] args) {
+
+        /*if (UserSession.getLoggedInUser() == null || UserSession.getLoggedInUser().getUserId() != userId) {
+            System.out.println("Gabim: Përdoruesi me ID " + userId + " nuk ekziston ose nuk është i loguar.");
+            return;
+        }*/
+        User adminUser = new User();
+        adminUser.setUserId(1);
+        adminUser.setUserName("admin");
+        adminUser.setRole("admin");
+        UserSession.setLoggedInUser(adminUser);
+
+        AdminService adminService = new AdminService();
+
+        /*if (!(UserSession.getLoggedInUser().getRole().equals("admin"))) {
+            System.out.println("Vetem administratoret mund t'i aksesojne keto te dhena!");
+        }*/
+
+        System.out.println("Testimi i metodes generateWeeklyFoodReport");
+        try {
+            Date startingDate = Date.valueOf("2024-12-28");
+            int userId = 2;
+            System.out.println(adminService.generateWeeklyFoodReport(userId, startingDate));
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        System.out.println("Testimi i metodes generateWeeklyFoodReport");
+        try {
+            Date startingDate = Date.valueOf("2024-12-28");
+            List<UserReport> users = adminService.usersWhoExceededMonthlySpendingLimit(startingDate);
+            System.out.println("Users who exceeded monthly spending limit:");
+            users.forEach(System.out::println);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        User user = new User();
+        user.setUserId(2);
+        user.setUserName("user");
+        user.setRole("user");
+        UserSession.setLoggedInUser(user);
+
+
+        System.out.println("\nTestojme metodat per nje user te thjeshte:");
+        try {
+            Date startingDate = Date.valueOf("2024-12-01");
+            adminService.generateWeeklyFoodReport(2, startingDate);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        try {
+            Date startingDate = Date.valueOf("2024-12-01");
+            adminService.usersWhoExceededMonthlySpendingLimit(startingDate);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+        }
+
