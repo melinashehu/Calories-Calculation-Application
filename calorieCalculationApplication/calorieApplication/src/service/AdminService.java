f5c8abdc561e029f184c023e37a3fabf987b37f1
unknown <aminasokoli0602@outlook.com>
2025-01-07 23:13:48 +0100

    implementimi dhe kalimi i metodave te veprimeve me te dhenat e raportit nga AdminReport tek AdminService

--- calorieCalculationApplication/calorieApplication/src/service/AdminService.java
+++ calorieCalculationApplication/calorieApplication/src/service/AdminService.java
@@ -9,6 +9,7 @@ import entity.Food;
 import entity.User;
 import login.UserSession;
 import java.sql.Date;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -44,7 +45,7 @@ public class AdminService {
 
     }
 
-    public List<AdminReport> usersWhoExceededMonthlySpendingLimit(Date startingDate){
+    public List<AdminReport> usersWhoExceededMonthlySpendingLimit(Date startingDate){ //testuar ne console, punon
         if(!isAdmin()){
             throw new SecurityException("You have no access to this information!");
         }
@@ -61,5 +62,54 @@ public class AdminService {
             }else System.out.println("No user has exceeded the monthly expenditure.");
         }
         return usersWhoExceededMonthlySpendingLimit;
+    }
+
+    /**
+     *
+     * @author: Amina
+     */
+    public List<User> getAvgCaloriesPerUserLast7Days(){ //punon ne console
+        List<User> reportList = new ArrayList<>();
+        LocalDate today = LocalDate.now();
+        Date startDate = Date.valueOf(today.minusDays(7));
+        try{
+            List<User> users = userDAO.getAllUsers();
+            for(User user : users){
+                List<Food> userFoods = foodDAO.getAllFoodsFromAWeeklyPeriodForAUser(user.getUserId(), startDate);
+                double totalCalories = userFoods.stream().mapToDouble(Food::getCalorie).sum();
+                double avgCalories = userFoods.isEmpty() ? 0.0 : totalCalories / userFoods.size();
+                reportList.add(new User(user.getUserName(), avgCalories));
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return reportList;
+    }
+
+    /**
+     *
+     * @author: Amina
+     */
+    public String printFoodEntriesPerWeekComparison(){
+        LocalDate today = LocalDate.now();
+        Date currentDate = Date.valueOf(today);
+        Date preivousWeekStartDate = Date.valueOf(today.minusWeeks(1));
+
+        int currentWeekEntries = foodDAO.getAllFoodEntriesForAWeeklyPeriod(currentDate);
+        int previousWeekEntries = foodDAO.getAllFoodEntriesForAWeeklyPeriod(preivousWeekStartDate);
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("Current week entries: ").append(currentWeekEntries);
+        builder.append("\nPrevious week entries: ").append(previousWeekEntries);
+
+        if(currentWeekEntries > previousWeekEntries){
+            builder.append("\nThis week has ").append((currentWeekEntries - previousWeekEntries)).append("more entries compared to the previous week.");
+        }else if(currentWeekEntries < previousWeekEntries){
+            builder.append("\nThis week has ").append((previousWeekEntries - currentWeekEntries)).append(" fewer entries compared to the previous week.");
+        }else{
+            builder.append("\nThe number of entries is the same as the previous week.");
+        }
+
+        return builder.toString();
     }
 }
