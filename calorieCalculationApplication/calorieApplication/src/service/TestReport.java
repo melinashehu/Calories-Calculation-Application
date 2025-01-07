a6ef419ccce82bb07bfb28c522b999106458e38c
unknown <aminasokoli0602@outlook.com>
2025-01-07 23:21:05 +0100

    testim i raportit

--- calorieCalculationApplication/calorieApplication/src/service/TestReport.java
+++ calorieCalculationApplication/calorieApplication/src/service/TestReport.java
@@ -1,6 +1,9 @@
 package service;
 
 import Report.AdminReport;
+import entity.*;
+
+import java.util.List;
 
 public class TestReport {
     public static void main(String[] args) {
@@ -25,7 +28,16 @@ public class TestReport {
         System.out.println("Kaloritë totale të konsumuara: " + report.getTotalCalories());
         System.out.println("Shpenzimet totale: " + report.getTotalSpendingMoney());
         System.out.println("Ditët mbi pragun e kalorive: " + report.getDaysAboveCalorieThreshold());*/
-        AdminReport report = new AdminReport();
-        report.compareFoodEntriesPerWeek();
+
+        AdminService report = new AdminService();
+       //report.printFoodEntriesPerWeekComparison();
+        List<User> reportList = report.getAvgCaloriesPerUserLast7Days();
+        if(reportList.isEmpty()){
+            System.out.println("No reports found");
+        }else{
+            for(User user : reportList){
+                System.out.println("User: "+user.getUserName()+", Avg Calories: "+user.getAvgCalories());
+            }
+        }
     }
 }
