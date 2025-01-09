7ad20087655a5f21e742be3cb79bc6c68f0d4abe
Edna Brahimaj <Edna.Brahimaj@fti.edu.al>
2025-01-09 19:57:02 +0100

    Implementimi i metodes getMoneySpentFromAUser

--- calorieCalculationApplication/calorieApplication/src/dao/StatisticalReportDAOImplementation.java
+++ calorieCalculationApplication/calorieApplication/src/dao/StatisticalReportDAOImplementation.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class StatisticalReportDAOImplementation implements StatisticalReportDAO {
     @Override
@@ -38,11 +40,12 @@ public class StatisticalReportDAOImplementation implements StatisticalReportDAO 
             return 0.0;
         }
 
-    public double calculateMonthlySpendingForAUser(int userId, java.sql.Date startingDate){
-        String sql = "SELECT SUM(food_price) AS totalSpending " +
-                "FROM user_foods " +
-                "WHERE user_id = ? " +
-                "AND date_consumed BETWEEN ? AND DATE_ADD(?, INTERVAL 1 MONTH)";
+    /**
+     * @author:Edna
+     */
+    public List<Double> getMoneySpentFromAUser(int userId, java.sql.Date startingDate){
+        String sql = "SELECT food_price FROM user_foods WHERE user_id = ? AND date_consumed BETWEEN ? AND DATE_ADD(?, INTERVAL 1 MONTH)";
+        List<Double> getSpendingForAUser = new ArrayList<>();
 
         try (Connection connection = DatabaseConnection.getConnection();
              PreparedStatement pstmt = connection.prepareStatement(sql)) {
@@ -51,13 +54,13 @@ public class StatisticalReportDAOImplementation implements StatisticalReportDAO 
             pstmt.setDate(3, startingDate);
             ResultSet rs = pstmt.executeQuery();
 
-            if (rs.next()) {
-                return rs.getDouble("totalSpending");
+            while (rs.next()) {
+                getSpendingForAUser.add(rs.getDouble("food_price"));
             }
         } catch (SQLException e) {
             System.err.println("Error calculating monthly spending for user: " + e.getMessage());
         }
-        return 0.0;
+        return getSpendingForAUser;
     }
 
 
