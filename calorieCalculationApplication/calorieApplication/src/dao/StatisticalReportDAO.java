ad94038f994e85e3bf564919140fcf7fcfd29d9f
Edna Brahimaj <Edna.Brahimaj@fti.edu.al>
2025-01-09 19:56:14 +0100

    Shtimi i metodes getMoneySpentFromAUser per te shmangur llogaritjet ne klasen StatisticalReportDAO

--- calorieCalculationApplication/calorieApplication/src/dao/StatisticalReportDAO.java
+++ calorieCalculationApplication/calorieApplication/src/dao/StatisticalReportDAO.java
@@ -1,8 +1,10 @@
 package dao;
 
+import java.util.List;
+
 public interface StatisticalReportDAO {
     double calculateTotalCaloriesConsumed(int userId);
     double calculateTotalSpendingMoney(int userId);
     int calculateDaysAboveCalorieThreshold(int userId, double calorieThreshold);
-    double calculateMonthlySpendingForAUser(int userId, java.sql.Date startingDate);
+    List<Double> getSpendingForAUser(int userId, java.sql.Date startingDate);
 }
